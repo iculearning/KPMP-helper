c1391c904ede762297fa77b36584adaea6317031
Sami Safadi <samisaf@gmail.com>
2023-10-31 16:11:57 -0500

    adjusts helper file

--- helper.py
+++ helper.py
@@ -1,8 +1,20 @@
 from dotenv import dotenv_values
+import os
 
 
 def get_key(key: str, path=""):
-    """Gets key from .env file"""
+    """Gets key from the environment or from a .env file"""
+
+    # first try to get the key if saved as an environment variable
+    result = os.environ.get(key)
+    if result:
+        return result
+    # if the key is not found in the environment variables, then search for it in a .env file
+    else:
+        return get_key_dotenv(key)
+
+
+def get_key_dotenv(key: str, path=""):
     paths = [
         "../.env",
         "./.env",
@@ -17,3 +29,8 @@ def get_key(key: str, path=""):
             if len(dotenv_values(p)) > 0:
                 return dotenv_values(p)[key]
     return None
+
+
+if __name__ == "__main__":
+    r1 = get_key("PINECONE_API_KEY")
+    print("Done")
